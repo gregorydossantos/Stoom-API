5c2e901ccd2014a895228595c8e9adfe46b302b9
Gregory <gregescribano@gmail.com>
2020-11-22 20:55:34 -0300

    Key removida, usada durante os testes apenas.

--- src/main/java/br/com/gregory/apirest/controllers/services/GeocondigApi.java
+++ src/main/java/br/com/gregory/apirest/controllers/services/GeocondigApi.java
@@ -3,6 +3,6 @@ package br.com.gregory.apirest.controllers.services;
 public class GeocondigApi {
 
     public static final String GOOGLE_ADDRES_API = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-    public static final String GOOGLE_API_KEY = "&key=AIzaSyDTK0igIQTCi5EYKL9tzOIJ9N6FUASGZos";
+    public static final String GOOGLE_API_KEY = "&key="; //Key removida, usada durante os testes apenas
 
 }
